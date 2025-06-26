10401b1edb6bf76d2abdbfcd4ba8e6a84f157865
Emerson Roncancio <roncancioavila@gmail.com>
2025-06-25 22:27:36 -0500

    contruccion y configuracion del proyecto

---
+++ src/app/store-front/store-front.routes.ts
@@ -0,0 +1,35 @@
+import { Routes } from '@angular/router';
+import { StoreFrontLayoutComponent } from './layouts/store-front-layout/store-front-layout.component';
+import { GenderPageComponent } from './pages/gender-page/gender-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: StoreFrontLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: HomePageComponent,
+      },
+      {
+        path: 'gender/:gender',
+        component: GenderPageComponent,
+      },
+      {
+        path: 'product/:productId',
+        component: ProductPageComponent,
+      },
+      {
+        path: '**',
+        component: NotFoundPageComponent,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
